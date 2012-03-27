c6b154ed113cea49c1ae4ee5052d7c99cbdd5e79
enum <GitHub@404notfound.jp>
2012-03-28 03:19:09 +0900

    all_customersで複数のユーザを扱えるようになったので、サンプルデータを追加

--- cookbooks/sample/attributes/default.rb
+++ cookbooks/sample/attributes/default.rb
@@ -2,5 +2,9 @@ mycompany_customers({
     :bob => {
         :homedir => "/home/vagrant/bob",
         :webdir => "/home/vagrant/bob/web"
+    },
+    :tom => {
+        :homedir => "/home/vagrant/tom",
+        :webdir => "/home/vagrant/tom/web"
     }
 })
